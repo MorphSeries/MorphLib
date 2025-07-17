75a56c3f3b2d04ea1484312217a469906512ffc1
morph <morphie108@gmail.com>
2025-07-17 10:17:10 -0700

    Initial Commit: The Start

---
+++ src/main/java/dev/morphie/morphLib/MorphLib.java
@@ -0,0 +1,17 @@
+package dev.morphie.morphLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MorphLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
