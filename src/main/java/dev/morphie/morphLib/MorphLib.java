6944ccc3d084531f7682e349ecd6951ee020cef2
morph <morphie108@gmail.com>
2025-07-17 11:12:35 -0700

    Added string colorizer and item stack maker.

--- src/main/java/dev/morphie/morphLib/MorphLib.java
+++ src/main/java/dev/morphie/morphLib/MorphLib.java
@@ -4,14 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MorphLib extends JavaPlugin {
 
+    public String Version;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Version = this.getDescription().getVersion();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ src/main/java/dev/morphie/morphLib/itemstack/ItemMaker.java
@@ -0,0 +1,31 @@
+package dev.morphie.morphLib.itemstack;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class ItemMaker {
+
+    public ItemStack makeItem(Material material, int amount, int modelid, String name, ArrayList<String> lore, Boolean glow, Boolean unbreakable) {
+        ItemStack item = new ItemStack(material, amount);
+        ItemMeta meta = item.getItemMeta();
+        meta.addItemFlags(new ItemFlag[] { ItemFlag.HIDE_ATTRIBUTES });
+        meta.addItemFlags(new ItemFlag[] { ItemFlag.HIDE_DESTROYS });
+        meta.setDisplayName(name);
+        meta.setLore(lore);
+        meta.setCustomModelData(modelid);
+        if (glow) {
+            meta.addEnchant(Enchantment.UNBREAKING, 1, true);
+            meta.addItemFlags(new ItemFlag[] { ItemFlag.HIDE_ENCHANTS });
+        }
+        if (unbreakable) {
+            meta.setUnbreakable(true);
+        }
+        item.setItemMeta(meta);
+        return item;
+    }
+}
