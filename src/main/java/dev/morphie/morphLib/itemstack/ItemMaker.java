6e4efa374c001efdb631055cf7a57817e4523069
morph <morphie108@gmail.com>
2025-08-05 15:40:05 -0700

    Added support to set NBT tags to items + hide tool tip.

--- src/main/java/dev/morphie/morphLib/itemstack/ItemMaker.java
+++ src/main/java/dev/morphie/morphLib/itemstack/ItemMaker.java
@@ -1,5 +1,7 @@
 package dev.morphie.morphLib.itemstack;
 
+import de.tr7zw.nbtapi.NBTCompound;
+import de.tr7zw.nbtapi.NBTItem;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -7,14 +9,12 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.xml.sax.ext.Attributes2;
 
-import javax.print.attribute.Attribute;
 import java.util.ArrayList;
 
 public class ItemMaker {
 
-    public ItemStack makeItem(Material material, int amount, int modelid, String name, ArrayList<String> lore, Boolean glow, Boolean unbreakable, Player p) {
+    public ItemStack makeItem(Material material, int amount, int modelid, String name, ArrayList<String> lore, Boolean glow, Boolean unbreakable, Player p, String nbt) {
         ItemStack item = new ItemStack(material, amount);
         if (material.equals(Material.PLAYER_HEAD)) {
             SkullMeta meta = (SkullMeta) item.getItemMeta();
@@ -29,6 +29,7 @@ public class ItemMaker {
             if (unbreakable) {
                 meta.setUnbreakable(true);
             }
+            meta.setHideTooltip(true);
             meta.setDisplayName(name);
             meta.setLore(lore);
             meta.setCustomModelData(modelid);
@@ -45,10 +46,17 @@ public class ItemMaker {
             if (unbreakable) {
                 meta.setUnbreakable(true);
             }
+            meta.setHideTooltip(true);
             meta.setDisplayName(name);
             meta.setLore(lore);
             meta.setCustomModelData(modelid);
             item.setItemMeta(meta);
+            if (nbt != null) {
+                NBTItem nbtItem = new NBTItem(item);
+                NBTCompound comp = nbtItem.addCompound(nbt);
+                return nbtItem.getItem();
+            }
+
         }
         return item;
     }
