4bf5646e17b812facd17748406eb17e57d1c333b
morph <morphie108@gmail.com>
2025-08-13 13:49:07 -0700

    Added Action bar message support.

--- src/main/java/dev/morphie/morphLib/utils/StringUtils.java
+++ src/main/java/dev/morphie/morphLib/utils/StringUtils.java
@@ -1,5 +1,9 @@
 package dev.morphie.morphLib.utils;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.entity.Player;
+
 public class StringUtils {
 
     public static String capitalize(String str) {
@@ -7,5 +11,9 @@ public class StringUtils {
             return str; // Handle null or empty strings
         }
         return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    public void ActionBar (String message, Player player ) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new ComponentBuilder(message).create());
     }
 }
